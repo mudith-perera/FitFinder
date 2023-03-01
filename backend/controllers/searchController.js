724e33604bb3173ff86c57de9e1c3b329c5ee5d1
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-03-01 11:17:56 +0530

    search controller

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,28 +1,84 @@
-/////////////////////////  Modified Date   : 07-02-2023     /////////////////////////
-/////////////////////////  Description     : 
+/////////////////////////  Modified Date   : 28-02-2023     /////////////////////////
+/////////////////////////  Description     : controller which give the search result
 
 //import mongoose
 const mongoose = require("mongoose");
 
 //importing the model
-const User = require("../models/userModel.js");
+const Gym = require("../models/gymModel");
 
 ////////////////////////////////////////    Controllers (START)   ////////////////////////////////////////
 
 /////////////////////////  Controller       : searchGym()
 /////////////////////////  Description      : 
-/////////////////////////  Developer        : 
+/////////////////////////  Developer        : Gimhani Harshika
 /////////////////////////  (START)
-const getUserEmailPwd = async (req, res) => {
-  return res.status(401).json({ message: "Controller is not created" });
+const getByGymName = async (req, res) => {
+  console.log("0");
+  //const { gymName } = req.params;
+  // const regex = new RegExp(gymName, "i");
+  console.log("1");
+  //const gyms = await Gym.find({ gymName }).sort({ createdAt: -1 });
+  console.log("2");
+  // const gyms = await Gym.find({gymName, location, gymSexType}).exec();
+
+
+  // if (!gyms) {
+  //   res.status(400).json({ error: "No Gyms in the system" });
+  // }
+  //   res.status(200).json(gyms);
+  
 };
+
+
+// const getByLocation = async (req, res) => {
+//   const { location } = req.params;
+//   const gyms = await Gym.find({ location }).sort({ createdAt: -1 });
+
+//   // const gyms = await Gym.find({gymName, location, gymSexType}).exec();
+
+//   if (!gyms.activeStatus) {
+//     res.status(400).json({ error: "No Gyms in the system" });
+//   } else {
+//     res.status(200).json(gyms);
+//   }
+// };
+
+
+// const GetByLocationAndSex = async (req, res) => {
+//   const { location, gymSexType } = req.params;
+//   const gyms = await Gym.find({ location, gymSexType }).sort({ createdAt: -1 });
+
+//   // const gyms = await Gym.find({gymName, location, gymSexType}).exec();
+
+//   if (!gyms.activeStatus) {
+//     res.status(400).json({ error: "No Gyms in the system" });
+//   } else {
+//     res.status(200).json(gyms);
+//   }
+// };
+
+
+// const GetByLocationAndGymName = async (req, res) => {
+//   const { location, gymName } = req.params;
+//   const gyms = await Gym.find({ location, gymName }).sort({ createdAt: -1 });
+
+//   // const gyms = await Gym.find({gymName, location, gymSexType}).exec();
+
+//   if (!gyms.activeStatus) {
+//     res.status(400).json({ error: "No Gyms in the system" });
+//   } else {
+//     res.status(200).json(gyms);
+//   }
+// };
+
 /////////////////////////  (END)
 
 ////////////////////////////////////////     Controllers (END)    ////////////////////////////////////////
 
 module.exports = {
-  createUser,
-  getUsers,
-  getUser,
-  getUserEmailPwd,
+  getByGymName,
+  // getByLocation,
+  // GetByLocationAndSex,
+  // GetByLocationAndGymName
 };
--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,1 +1,53 @@
 //// gimhani Harshika
+
+//Create routes using express package
+const express = require("express");
+
+//importing the controller
+const {
+    getByGymName,
+    // getByLocation,
+    // GetByLocationAndSex,
+    // GetByLocationAndGymName
+} = require("../controllers/searchController");
+
+//get the Router instatnce from the express package
+const router = express.Router();
+////////////////////////////////////////     Request Handlers (START)    ////////////////////////////////////////
+
+
+/////////////////////////  Handler          : /
+/////////////////////////  Controller       : getSearchGym()
+/////////////////////////  Description      : get search gym
+/////////////////////////  Developer        : Gimhani Harshika
+/////////////////////////  (START)
+router.get("/", getByGymName); 
+/////////////////////////  (END)
+
+/////////////////////////  Handler          : /
+/////////////////////////  Controller       : getSearchGym()
+/////////////////////////  Description      : get search gym
+/////////////////////////  Developer        : Gimhani Harshika
+/////////////////////////  (START)
+// router.get("/search-gym-location", getByLocation);
+/////////////////////////  (END)
+
+/////////////////////////  Handler          : /
+/////////////////////////  Controller       : getSearchGym()
+/////////////////////////  Description      : get search gym
+/////////////////////////  Developer        : Gimhani Harshika
+/////////////////////////  (START)
+// router.get("/search-gym-location-sex", GetByLocationAndSex);
+/////////////////////////  (END)
+
+/////////////////////////  Handler          : /
+/////////////////////////  Controller       : getSearchGym()
+/////////////////////////  Description      : get search gym
+/////////////////////////  Developer        : Gimhani Harshika
+/////////////////////////  (START)
+// router.get("/search-gym-location-name", GetByLocationAndGymName);
+/////////////////////////  (END)
+
+//export the created routes
+module.exports = router;
+
