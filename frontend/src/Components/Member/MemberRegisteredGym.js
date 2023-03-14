c18d42a69e6e13e1e7c9e975eb6c4bd1075342a9
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-03-14 05:39:30 +0530

    User registered gym is done

--- frontend/src/Components/Member/MemberRegisteredGym.js
+++ frontend/src/Components/Member/MemberRegisteredGym.js
@@ -2,7 +2,7 @@
 ///////////////////////// Modified Date   : 19-02-2023     /////////////////////////
 /////////////////////////           (START)                /////////////////////////
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./UserGymHome.css";
 
 import Carousel from "react-bootstrap/Carousel";
@@ -12,168 +12,179 @@ import Table from "react-bootstrap/Table";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 import gym from "../../Images/gym.png";
-import first from "../../Images/gym1.png";
-import second from "../../Images/gym2.png";
-import third from "../../Images/gym3.png";
-import fourth from "../../Images/gym4.jpg";
-import fifth from "../../Images/gym5.png";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { useCookies } from 'react-cookie';
 
 import SideNavbar from "../Shared/SideNavbar.js";
 
 const MemberRegisteredGym = () => {
-  const cardInfo = [
-    {
-      GymName: "Max Fitness",
-      GymOwnerName: "Mr. Mudith Perera",
-      GymOwnerEmail: "mudithperera@gmail.com",
-      ContactNo1: "0700000010",
-      ContactNo2: "0110000101",
-      Location: "Matara",
-      Address: "No:51/2, Uyanwatta, Matara",
-      MonthFee: "15000",
-      AnnualFee: "55000",
-    },
-  ];
+  useEffect(() => {
+    Aos.init({ duration: 500 });
+  }, []);
 
-  // console.log(RegisteredGymInfo);
+  //user account create success alert
+  const userSuccess = () => {
+    toast.success("User Successfully Removed 😊👍", {
+      theme: "colored",
+      position: toast.POSITION.TOP_LEFT,
+    });
+  };
+
+  const [cookie] = useCookies(['']);
+  const [GymName, setGymName] = useState(cookie.LoggedUser[6].gymName);
+  const [GymOwnerName, setGymOwnerName] = useState(cookie.LoggedUser[6].gymOwnerName);
+  const [GymOwnerEmail, setGymOwnerEmail] = useState(cookie.LoggedUser[6].email);
+  const [ContactNo1, setContactNo1] = useState(cookie.LoggedUser[6].gymContactNo1);
+  const [ContactNo2, setContactNo2] = useState(cookie.LoggedUser[6].gymContactNo2);
+  const [Location, setLocation] = useState(cookie.LoggedUser[6].location);
+  const [Address, setAddress] = useState(cookie.LoggedUser[6].gymAddress);
+  const [MonthFee, setMonthFee] = useState(cookie.LoggedUser[6].gymMonthlyFee);
+  const [AnnualFee, setAnnualFee] = useState(cookie.LoggedUser[6].gymAnnualFee);
+  const [GymImages, setGymImages] = useState(cookie.LoggedUser[6].images);
+
+  // console.log(cookie);
+
+  const registeredGymActivateStatus = false;
+  const userId = cookie.LoggedUser[5];
+  const gymRegistered = async () => {
+    const formData = { registeredGymActivateStatus };
+
+    //user remove backend
+    const response = await fetch("/api/users/" + userId, {
+      method: "PATCH",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      userSuccess();
+      console.log("new user added", json);
+    }
+  };
 
   return (
     <div>
+      <ToastContainer />
       <div style={{ position: "fixed", zIndex: "1" }}>
-        <SideNavbar userRole="member" />
+        <SideNavbar userRole={cookie.LoggedUser[0]} />
       </div>
-      <div className="container py-5 px-5">
-        <div>
-          <div className="card ">
-            <div className="card-header">
-              <h4 className="card-title">Member Currently Registered Gym</h4>
-              <img
-                src={gym}
-                alt="default pic"
-                height="50px"
-                className="card-defaultimage"
-              />
-            </div>
-            <div className="card-body">
-              <Table>
-                <tr>
-                  <td>
-                    <Table>
-                      <thead>
-                        <tr>
-                          <th
-                            style={{
-                              width: 130,
-                            }}
-                          >
-                            Gym Name
-                          </th>
-                          <td>{cardInfo[0].GymName}</td>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th>Gym Owner's Name</th>
-                          <td>{cardInfo[0].GymOwnerName}</td>
-                        </tr>
-                        <tr>
-                          <th>Gym Owner's E-mail</th>
-                          <td>{cardInfo[0].GymOwnerEmail}</td>
-                        </tr>
-                        <tr>
-                          <th>Contact No 1</th>
-                          <td>{cardInfo[0].ContactNo1}</td>
-                        </tr>
-                        <tr>
-                          <th>Contact No 2</th>
-                          <td>{cardInfo[0].ContactNo2}</td>
-                        </tr>
-                        <tr>
-                          <th>Location</th>
-                          <td>{cardInfo[0].Location}</td>
-                        </tr>
-                        <tr>
-                          <th>Address</th>
-                          <td>{cardInfo[0].Address}</td>
-                        </tr>
-                        <tr>
-                          <th>Monthly Fee</th>
-                          <td>{cardInfo[0].MonthFee}</td>
-                        </tr>
-                        <tr>
-                          <th>Annual Fee</th>
-                          <td>{cardInfo[0].AnnualFee}</td>
-                        </tr>
-                      </tbody>
-                    </Table>
-                  </td>
-                  <td
-                    style={{
-                      width: 650,
-                    }}
-                  >
-                    <Carousel className="py-2">
-                      <Carousel.Item>
-                        <img
-                          className="d-block"
-                          src={first}
-                          alt="First slide"
-                          style={{ width: "650px", height: "430px" }}
-                        />
-                        <Carousel.Caption></Carousel.Caption>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <img
-                          className="d-block"
-                          src={second}
-                          alt="Second slide"
-                          style={{ width: "650px", height: "430px" }}
-                        />
-                        <Carousel.Caption></Carousel.Caption>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <img
-                          className="d-block"
-                          src={third}
-                          alt="Third slide"
-                          style={{ width: "650px", height: "430px" }}
-                        />
-                        <Carousel.Caption></Carousel.Caption>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <img
-                          className="d-block"
-                          src={fourth}
-                          alt="Fourth slide"
-                          style={{ width: "650px", height: "430px" }}
-                        />
-                        <Carousel.Caption></Carousel.Caption>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <img
-                          className="d-block"
-                          src={fifth}
-                          alt="Fifth slide"
-                          style={{ width: "650px", height: "430px" }}
-                        />
-                        <Carousel.Caption></Carousel.Caption>
-                      </Carousel.Item>
-                    </Carousel>
-                  </td>
-                </tr>
-              </Table>
-              <Button
-                variant="contained"
-                type="submit"
-                color="error"
-                endIcon={<DeleteIcon />}
-              >
-                Remove Me
-              </Button>
+      {cookie.LoggedUser[6].activeStatus ?
+
+        <div className="container py-5 px-5">
+          <div>
+            <div className="card ">
+              <div className="card-header">
+                <h4 className="card-title">{cookie.LoggedUser[0]} Currently Registered Gym</h4>
+                <img
+                  src={gym}
+                  alt="default pic"
+                  height="50px"
+                  className="card-defaultimage"
+                />
+              </div>
+              <div className="card-body">
+                <Table>
+                  <tr>
+                    <td>
+                      <Table>
+                        <thead>
+                          <tr>
+                            <th
+                              style={{
+                                width: 130,
+                              }}
+                            >
+                              Gym Name
+                            </th>
+                            <td>{GymName}</td>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th>Gym Owner's Name</th>
+                            <td>{GymOwnerName}</td>
+                          </tr>
+                          <tr>
+                            <th>Gym Owner's E-mail</th>
+                            <td>{GymOwnerEmail}</td>
+                          </tr>
+                          <tr>
+                            <th>Contact No 1</th>
+                            <td>{ContactNo1}</td>
+                          </tr>
+                          <tr>
+                            <th>Contact No 2</th>
+                            <td>{ContactNo2}</td>
+                          </tr>
+                          <tr>
+                            <th>Location</th>
+                            <td>{Location}</td>
+                          </tr>
+                          <tr>
+                            <th>Address</th>
+                            <td>{Address}</td>
+                          </tr>
+                          <tr>
+                            <th>Monthly Fee</th>
+                            <td>{MonthFee}</td>
+                          </tr>
+                          <tr>
+                            <th>Annual Fee</th>
+                            <td>{AnnualFee}</td>
+                          </tr>
+                        </tbody>
+                      </Table>
+                    </td>
+                    <td
+                      style={{
+                        width: 650,
+                      }}
+                    >
+
+                      <Carousel className="py-2">
+                        {GymImages ?
+                          GymImages.map((url, index) => (
+                            <Carousel.Item key={index}>
+                              <img
+                                key={index}
+                                src={url}
+                                alt={`Image ${index}`}
+                                style={{ width: "650px", height: "430px" }}
+                              />
+                              <Carousel.Caption></Carousel.Caption>
+                            </Carousel.Item>
+                          ))
+                          : <></>
+                        }
+                      </Carousel>
+                    </td>
+                  </tr>
+                </Table>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  color="error"
+                  endIcon={<DeleteIcon />}
+                  onClick={() => gymRegistered()}
+                >
+                  Remove Me
+                </Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        : <p>No Gym still you registered</p>}
+
     </div>
   );
 };
