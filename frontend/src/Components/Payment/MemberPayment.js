254cd6a7d1b792fb5fe059cc033180ac4f486ce0
mudith-perera <makawitagemudith@gmail.com>
2023-04-04 20:06:53 +0530

    Fine Tunning 1

--- frontend/src/Components/Payment/MemberPayment.js
+++ frontend/src/Components/Payment/MemberPayment.js
@@ -29,75 +29,102 @@ const MemberPayment = () => {
     cookie.LoggedUser[2] + " " + cookie.LoggedUser[3]
   );
   const [useremail] = useState(cookie.LoggedUser[4]);
-  const monthlyFee = useState(cookie.LoggedUser[6].gymMonthlyFee);
-  const annualFee = useState(cookie.LoggedUser[6].gymAnnualFee);
-  
+  const [gymDetails] = useState(cookie.LoggedUser[6]);
+  const monthlyFee = useState(cookie.LoggedUser[6]?.gymMonthlyFee);
+  const annualFee = useState(cookie.LoggedUser[6]?.gymAnnualFee);
+
   return (
     <div>
       <div style={{ position: "fixed", zIndex: "1" }}>
         <SideNavbar userRole="member" />
       </div>
-      <section data-aos="flip-right" className="vh-800 gradient-custom">
-        {/* <ToastContainer /> */}
-        <div className="container py-5 h-80">
-          <div className="row d-flex justify-content-center align-items-center h-800">
-            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div
-                className="card bg-dark text-white"
-                style={{ borderRadius: "1rem" }}
-              >
-                <div className="card-body p-3 text-center">
-                  <div className="mb-md-2 mt-md-4 pb-2">
-                    <h2 className="fw-bold mb-2 text-uppercase">
-                      Payment Checkout
-                    </h2>
-                    <p className="text-white-50 mb-5">
-                      Please Select a Payment Option below
-                    </p>
-                  </div>
-                  <div>
-                    <p>Pay Monthly Rs : {monthlyFee}</p>
-                    <PayButton
-                      username={username}
-                      useremail={useremail}
-                      fee={monthlyFee}
-                    />
-                  </div>
-                  <div>
-                    <p>Pay Annualy Rs : {annualFee}</p>
-                    <PayButton
-                      username={username}
-                      useremail={useremail}
-                      fee={annualFee}
-                    />
-                  </div>
+      {gymDetails ? (
+        <section data-aos="flip-right" className="vh-800 gradient-custom">
+          <div className="container py-5 h-80">
+            <div className="row d-flex justify-content-center align-items-center h-800">
+              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div
+                  className="card bg-dark text-white"
+                  style={{ borderRadius: "1rem" }}
+                >
+                  <div className="card-body p-3 text-center">
+                    <div className="mb-md-2 mt-md-4 pb-2">
+                      <h2 className="fw-bold mb-2 text-uppercase">
+                        Payment Checkout
+                      </h2>
+                      <p className="text-white-50 mb-5">
+                        Please Select a Payment Option below
+                      </p>
+                    </div>
+                    <div>
+                      <p>Pay Monthly Rs : {monthlyFee}</p>
+                      <PayButton
+                        username={username}
+                        useremail={useremail}
+                        fee={monthlyFee}
+                      />
+                    </div>
+                    <div>
+                      <p>Pay Annualy Rs : {annualFee}</p>
+                      <PayButton
+                        username={username}
+                        useremail={useremail}
+                        fee={annualFee}
+                      />
+                    </div>
 
-                  <div
-                    id="signInDiv"
-                    style={{
-                      textAlign: "center",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  ></div>
-                  <div>
-                    <p className="mb-2 mt-md-5">
-                      Don't have an Stripe account?{" "}
-                      <a
-                        href="https://stripe.com/"
-                        className="text-white-50 fw-bold"
-                      >
-                        Goto Stripes
-                      </a>
-                    </p>
+                    <div
+                      id="signInDiv"
+                      style={{
+                        textAlign: "center",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    ></div>
+                    <div>
+                      <p className="mb-2 mt-md-5">
+                        Don't have an Stripe account?{" "}
+                        <a
+                          href="https://stripe.com/"
+                          className="text-white-50 fw-bold"
+                        >
+                          Goto Stripes
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <section data-aos="fade-right" className="vh-800 gradient-custom">
+          <div className="container py-5 h-80">
+            <div className="row d-flex justify-content-center align-items-center h-600">
+              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div className="card bg-white" style={{ borderRadius: "1rem" }}>
+                  <div className="card-body p-3 text-center">
+                    <div className="mb-md-2 mt-md-4 pb-2">
+                      <br />
+                      <br />
+                      <h2 className="fw-bold mb-2 text-uppercase">
+                        You haven't Registered for a gym 😁
+                      </h2>
+                      <p>You can registered for a gym by searching...</p>
+                      <br />
+                    </div>
+
+                    <p className="large text-white-50">Or</p>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </div>
   );
 };
