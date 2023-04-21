18ba78b31434a5569b2665f118ca8595042eee90
mudith-perera <makawitagemudith@gmail.com>
2023-04-21 20:18:46 +0530

    coach view and update done

--- frontend/src/Components/Shared/SideNavbar.js
+++ frontend/src/Components/Shared/SideNavbar.js
@@ -10,7 +10,7 @@ import { CDBSidebarMenuItem } from "cdbreact";
 
 import { MdManageAccounts, MdScheduleSend, MdPayments } from "react-icons/md";
 import { AiFillSchedule } from "react-icons/ai";
-import { FaUsers, FaUserEdit, FaUserPlus, FaBars } from "react-icons/fa";
+import { FaUsers, FaUserEdit, FaUserPlus, FaBars,FaSearch } from "react-icons/fa";
 
 let iconStyle = { fontWeight: "600", color: "#fff", fontSize: "2rem" };
 
@@ -21,7 +21,7 @@ export const MenuItems = [
         NavsArray: [
             {
                 text: 'Search For Gyms',
-                icon: <FaUserEdit style={iconStyle} />,
+                icon: <FaSearch style={iconStyle} />,
                 path: "/",
             },
             {
@@ -54,7 +54,7 @@ export const MenuItems = [
         NavsArray: [
             {
                 text: 'Search For Gyms',
-                icon: <FaUserEdit style={iconStyle} />,
+                icon: <FaSearch style={iconStyle} />,
                 path: "/",
             },
             {
